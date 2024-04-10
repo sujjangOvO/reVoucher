6461cba77f82f41d2c65b594da78ea949b599b28
jangsuyeon <pnk7038@gmail.com>
2024-04-10 21:37:22 +0900

    init: project init

---
+++ src/main/java/com/example/revoucher/ReVoucherApplication.java
@@ -0,0 +1,13 @@
+package com.example.revoucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReVoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReVoucherApplication.class, args);
+	}
+
+}
